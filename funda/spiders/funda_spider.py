4d812c721d1321f0767d647830b8e34641e108e6
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2021-01-08 00:13:51 +0100

    scrape pages recursively until no next available

--- funda/spiders/funda_spider.py
+++ funda/spiders/funda_spider.py
@@ -1,18 +1,23 @@
 import scrapy
 
-
 class FundaSpider(scrapy.Spider):
     name = "funda"
 
     start_urls = [
-        'https://www.funda.nl/en/koop/amsterdam/p1/',
-        'https://www.funda.nl/en/koop/amsterdam/p2/',
+        'https://www.funda.nl/en/koop/gouda/'
     ]
 
     def parse(self, response):
+        # construct json from html source response
         for item in response.css('div.search-result-content'):
             yield {
                 'address': item.css('h2.search-result__header-title::text').get(),
                 'price': item.css('span.search-result-price::text').get(),
             }
+
+        # scrape recursively until next page not available
+        next_page = response.css('[rel=next]::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
